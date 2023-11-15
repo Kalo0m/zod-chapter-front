fab2a1e218378595b02c927171ecc4f5db6e586c
Théo Letouzé <theo.letouze@abtasty.com>
2023-11-15 17:47:58 +0100

    add zod and spot errors

--- packages/client/src/api.ts
+++ packages/client/src/api.ts
@@ -1,28 +1,28 @@
+import { z } from "zod";
+
+const ExperimentSchema = z.object({
+  id: z.string(),
+  name: z.string(),
+  description: z.string().optional(),
+  tags: z.array(z.object({
+    name: z.string(),
+    color: z.string()
+  })).optional(),
+  group: z.object({
+    id: z.string(),
+    name: z.string()
+  }).optional(),
+  parent: z.string().optional()
+})
+
+export type Experiment = z.infer<typeof ExperimentSchema>
+
 const API_URL = 'http://localhost:3000'
-
-export type Tag = {
-  name: string,
-  color: string
-}
-
-export type Group = {
-  id: string
-  name: string
-}
-
-export type Experiment = {
-  id: string
-  name: string
-  description?: string
-  tags?: Tag[]
-  group?: Group
-  parent?: string
-}
 
 export const apiClient = {
   getExperiments: async () => {
     const response = await fetch(`${API_URL}/experiments`)
-    const experiments = await response.json() as Experiment[]
-    return experiments
+    const experiments = await response.json()
+    return ExperimentSchema.array().parse(experiments)
   },
 }
